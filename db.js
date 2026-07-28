31fb5a924d3f61eaf00ce609d15962578c7bafb5
Suman Kalyan Jana <sumankalyan0514@gmail.com>
2026-07-28 15:44:30 +0530

    step 5 - added schema and connected to database

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const courseSchema = new Schema({
+    title : String,
+    decription: String,
+    price: Number,
+    imageUrl: String,
+    createrId: ObjectId
+})
+
+const purchaseSchema = new Schema({
+    userId : ObjectId,
+    courseId : ObjectId
+})
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const { userRouter } = require("./routes/user")
 const { courseRouter } = require("./routes/course")
 const { adminRouter } = require("./routes/admin")
@@ -8,4 +11,9 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
-app.listen(3000);
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URI);
+    app.listen(3000);
+}
+
+main();
